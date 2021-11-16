1baf05a5536332f03087e904eda33f1ba5f35218
William <cwpan@users.noreply.github.com>
2021-11-16 00:43:52 -0500

    added html page

--- test.py
+++ test.py
@@ -1,26 +1,31 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict',methods=['POST'])
 def predict():
     import requests
-    
+    '''
+    For rendering results on HTML page
+    '''
+    fl_features = [float(x) for x in request.form.values()]
+    fltdata = [np.array(fl_features)]
+    payload="{\"data\":\"fltdata\"}"
     url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
-    payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
+    #payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
     headers = {'Content-Type': 'application/json'}
 
     response = requests.request("POST", url, headers=headers, data=payload)
     name = str(response.text)
-    #return render_template("predict.html",name=name)
-    return name
-
+    #return render_template('index.html', prediction_text=name)
+    return render_template('index.html', prediction_text=fltdata)
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port='8080')
