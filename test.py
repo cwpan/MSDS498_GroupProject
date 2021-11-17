08ed20e114cb860636b6e46eb05b4fe48b20f962
William <cwpan@users.noreply.github.com>
2021-11-17 02:22:32 -0500

    string conversion

--- test.py
+++ test.py
@@ -16,7 +16,8 @@ def predict():
     '''
     fl_features = [float(x) for x in request.form.values()]
     fltdata = [np.array(fl_features)]
-    payload="{\"data\":\"fltdata\"}"
+    strdata=fltdata.tolist()
+    payload="{\"data\":strdata}"
     url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
     #payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
@@ -24,8 +25,8 @@ def predict():
 
     response = requests.request("POST", url, headers=headers, data=payload)
     name = str(response.text)
-    #return render_template('index.html', prediction_text=name)
-    return render_template('index.html', prediction_text=fltdata)
+    return render_template('index.html', prediction_text=name)
+    #return render_template('index.html', prediction_text=fltdata)
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port='8080')
