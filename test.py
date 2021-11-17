86c7813e7ec0f16272cdae9512685507dad05321
William <cwpan@users.noreply.github.com>
2021-11-17 03:20:37 -0500

    Update test.py

--- test.py
+++ test.py
@@ -16,10 +16,7 @@ def predict():
     '''
     fl_features = [float(x) for x in request.form.values()]
     fltdata = [np.array(fl_features)]
-    strdata=','
-    for data1 in fltdata:
-       strdata +=data1
-    return strdata
+    strdata=str(fltdata)
     payload="{\"data\":strdata}"
     url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
