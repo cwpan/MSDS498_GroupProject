4445d5d669c6ab012ca9e370b465fab2d968228f
William <cwpan@users.noreply.github.com>
2021-11-18 00:08:55 -0500

    Update test.py

--- test.py
+++ test.py
@@ -17,8 +17,10 @@ def predict():
     '''
     fl_features = [str(x) for x in request.form.values()]
     strdata=','.join(fl_features)
-    jsondata={"data":strdata}
-    payload="{\"data\":\"\+strdata\+\"}"
+    dictdata={"data":strdata}
+    jsondata=json.dumps(dictdata)
+    payload=josn.loads(jsondata)
+    #payload="{\"data\":\"\+strdata\+\"}"
     url = "https://6l6l7n7vtb.execute-api.us-east-2.amazonaws.com/test/predict"
 
     #payload="{\"data\":\"0,-0.34147611300851444,0.18202662446267728,310000.0,2.0,2.0,1.0,44.0,-1.0,-1.0,-2.0,-2.0,-2.0,-2.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0\"}"
@@ -27,7 +29,7 @@ def predict():
     #response = requests.request("POST", url, headers=headers, data=payload)
     #name = str(response.text)
     #return render_template('index.html', prediction_text=name)
-    return render_template('index.html', prediction_text=jsondata)
+    return render_template('index.html', prediction_text=payload)
 	
 if __name__ == "__main__":
     app.debug = True
